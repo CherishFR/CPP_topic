52c76d84f7bde0cfcb6986ea6562b3ea061f678e
unknown <471151015@qq.com>
2019-03-18 20:31:45 +0800

    19/3/18

---
+++ day3/04构造函数的调用规则/04构造函数的调用规则.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+public:
+	// MyClass()
+	// {
+	// 	cout << "默认构造函数" << endl;
+	// }
+	MyClass(int a)
+	{
+		cout << "有参构造函数" << endl;
+	}
+};
+
+// 当我们提供了有参构造函数，系统就不会提供默认构造函数，但会提供默认的拷贝构造
+void test01()
+{
+	// MyClass c1;
+	MyClass c1(1);
+	MyClass c2(c1);
+}
+
+int main()
+{
+	test01();
+}
+
---
+++ day3/06初始化列表/06初始化列表.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+	Person()
+	{
+
+	}
+	/*
+	Person(int A, int B, int C)
+	{
+		m_A = A;
+		m_B = B;
+		m_C = C;
+	}
+	*/
+
+	// 利用初始化列表来初始化数据
+	Person(int A, int B, int C):m_A(A), m_B(B), m_C(C)
+	{}
+
+	int m_A;
+	int m_B;
+	int m_C;
+};
+
+void test01()
+{
+	Person p1(10, 24, 64);
+}
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
